62f139fe2dbc152c7301a16c35abc73c319642d1
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-04-17 15:31:44 +0300

    formos ir ju value

--- 030_DOM/app.js
+++ 030_DOM/app.js
@@ -30,8 +30,50 @@ document.querySelector("button.cb").addEventListener("click", (_) => {
 document.querySelector("button.ra").addEventListener("click", (_) => {
   document.querySelectorAll('[type="radio"]').forEach((c) => {
     if (c.checked) {
-      // su chekboxu savybe checked
       console.log(c.value);
+    }
+  });
+});
+
+document.querySelector("button.color").addEventListener("click", (_) => {
+  const value = document.querySelector('[type="color"]').value;
+  document.querySelector(".box-color").style.backgroundColor = value;
+});
+
+document.querySelector("button.ta").addEventListener("click", (_) => {
+  const value = document.querySelector("textarea").value;
+  console.log(value);
+});
+
+//uzdv
+const colors = ["skyblue", "orange", "crimson", "gray"];
+
+let colorsSelect = document.querySelector(".my-colors");
+
+let text;
+
+for (let i = 0; i < colors.length; i++) {
+  text += `<option value="${colors[i]}">${colors[i]}</option>`; //blogas kodas nes value vienodas
+}
+
+colorsSelect.innerHTML = "<select>" + text + "</select>";
+
+document.querySelector(".go").addEventListener("click", (_) => {
+  const value = document.querySelector(".my-colors select").value;
+  document.querySelector(".go").style.backgroundColor = value;
+});
+
+let h3 = document.querySelector("h3").innerText;
+let sum = 0;
+document.querySelector(".add").addEventListener("click", (_) => {
+  document.querySelectorAll('.digits [type="radio"]').forEach((c) => {
+    if (c.checked) {
+      let num = parseInt(c.value);
+
+      sum = sum + num;
+
+      document.querySelector("h3").innerText = sum;
+      console.log(sum);
     }
   });
 });
