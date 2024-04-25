0610d67cb5c4c2a67b1fdc494e5fed91954e1baa
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-04-25 15:32:11 +0300

    API, serveris,

---
+++ 035_serveris/app.js
@@ -0,0 +1,29 @@
+console.log("API");
+
+window.addEventListener("load", (_) => {
+  const ul = document.querySelector(".users");
+
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((response) => response.json())
+  //     .then((users) => {
+  //       console.log(users);
+  //       let usersLi = "";
+
+  //       users.forEach((u) => {
+  //         usersLi += "<li>" + u.name + "</li>";
+  //       });
+
+  //       ul.innerHTML = usersLi;
+  //     });
+
+  axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
+    console.log(response);
+    let usersLi = "";
+
+    response.data.forEach((u) => {
+      usersLi += "<li>" + u.company.catchPhrase + "</li>";
+    });
+
+    ul.innerHTML = usersLi;
+  });
+});
