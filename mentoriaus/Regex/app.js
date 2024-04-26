c60cdd4b618ebe591f9a4f0104e1a3a95b3bfbc4
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-04-26 12:01:39 +0300

    vowels

---
+++ mentoriaus/Regex/app.js
@@ -0,0 +1,21 @@
+const word = document.querySelector(".input-text");
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".results");
+
+btn.addEventListener("click", countVowel);
+
+function countVowel() {
+  let count = 0;
+  let wordVal = word.value.toLowerCase();
+  let regex = /^[aeiou]$/i;
+
+  for (let i = 0; i < wordVal.length; i++) {
+    const letter = wordVal[i];
+    if (letter.match(regex)) {
+      count++;
+      console.log(count);
+    }
+  }
+  result.innerHTML = `${wordVal} has ${count} vowels`;
+}
+
