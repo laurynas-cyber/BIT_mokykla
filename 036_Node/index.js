8cfad22e24501a57e53e2df059411e5fc6c8b8e5
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-04-30 15:34:04 +0300

    webpackas

--- 036_Node/index.js
+++ 036_Node/index.js
@@ -1,13 +1,8 @@
 const express = require("express"); //pareikalavimas is bibliotekos
 const app = express();
 const port = 80; //koki porta pasileisti
-
+app.use(express.static("public")); //registruojam statinius failus
 //Router
-
-app.get("/", (req, res) => {
-  const content = "<h1>bla bla</h1>";
-  res.send(content);
-});
 
 app.get("/bebras", (req, res) => {
   const content = "<h1>labas Bebrai</h1>";
@@ -18,9 +13,61 @@ app.get("/bebras/jonas", (req, res) => {
   const content = "<h1>labas Bebrai vardu Jonai</h1>";
   res.send(content);
 });
-app.get("/zveris/:animal", (req, res) => { //kintamasis po :
+app.get("/zveris/:animal", (req, res) => {
+  //kintamasis po :
   const animal = req.params.animal; //params
   const content = "<h1>labas " + animal + "</h1>";
+  res.send(content);
+});
+
+app.get("/sum/:num/:num1", (req, res) => {
+  //kintamasis po :
+  let sum = 0;
+  const num = parseInt(req.params.num); //params
+  let num1 = parseInt(req.params.num1); //params
+  sum = num + num1;
+  const content = "<h1>Suma:" + sum + "</h1>";
+  res.send(content);
+});
+
+app.get("/sum/:num/plus/:num1", (req, res) => {
+  const num = parseInt(req.params.num); //params
+  let num1 = parseInt(req.params.num1); //params
+  sum = num + num1;
+  const content = "<h1>Suma:" + sum + "</h1>";
+  res.send(content);
+});
+
+app.get("/calc/:num/:action/:num1", (req, res) => {
+  let action = req.params.action;
+  const num = parseInt(req.params.num);
+  let num1 = parseInt(req.params.num1);
+  if (action == "plus") {
+    sum = num + num1;
+  } else if (action == "minus") {
+    sum = num - num1;
+  }
+
+  const content = "<h1>Suma:" + sum + "</h1>";
+  res.send(content);
+});
+
+// http://localhost/narve?sedi=karve&guli=zuikis
+app.get("/narve", (req, res) => {
+  const kas = req.query.sedi; //perdavimas per query
+  const kas2 = req.query.guli;
+  const content = "<h1>Sedi:" + kas + "</h1>" + "<h1>guli:" + kas2 + "</h1>";
+  res.send(content);
+});
+
+// localhost/sumatorius?num=5&num2=8
+
+app.get("/sumatorius", (req, res) => {
+  let rez;
+  const num = parseInt(req.query.num);
+  const num2 = parseInt(req.query.num2);
+  rez = num + num2;
+  const content = "<h1>Sum:" + rez + "</h1>";
   res.send(content);
 });
 
--- task_02_Bankas/app.js
+++ task_02_Bankas/app.js
@@ -5,6 +5,20 @@ window.addEventListener("load", (_) => {
   const storeButton = FormModal.querySelector(".--submit");
   const LAST_ID = "AccLastSavedId";
   const ACC_LIST_LS = "AllAccList";
+  let ClientList = document.querySelector(".clients-list");
+  let html = `
+  <div class="info-line client">
+  <div class="number">{{id}}</div>
+  <div class="name">{{AccName}}</div>
+  <div class="surname">{{Surname}}</div>
+  <div class="sum">{{Total}}</div>
+  <div class="btnContainer">
+    <button class="add clientbtn">Add funds</button>
+    <button class="add clientbtn">Withdraw funds</button>
+    <button class="remove clientbtn">Delete Account</button>
+  </div>
+</div>
+  `;
 
   //localhost
   const getId = (_) => {
@@ -17,18 +31,38 @@ window.addEventListener("load", (_) => {
     return parseInt(id) + 1;
   };
 
+  const sortLocalHostData = (data) => {
+    data.sort((a, b) => {
+      const nameA = a.AccName.toUpperCase(); // ignore upper and lowercase
+      const nameB = b.AccName.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+
+      return 0;
+    });
+    return data;
+  };
+
   const read = (_) => {
-    const data = localStorage.getItem(ACC_LIST_LS);
+    let data = localStorage.getItem(ACC_LIST_LS);
+
     if (null === data) {
       return [];
     }
-    return JSON.parse(data);
+    data = sortLocalHostData(JSON.parse(localStorage.getItem(ACC_LIST_LS)));
+
+    return data;
   };
 
   const storeLocalHost = (_) => {
     const getLocalHostData = read();
     const formData = getForm();
     getLocalHostData.push(formData);
+    localStorage.setItem(ACC_LIST_LS, JSON.stringify(getLocalHostData));
     hideModal(FormModal);
   };
   //localhost ----
@@ -60,6 +94,20 @@ window.addEventListener("load", (_) => {
     });
     modal.style.display = "none";
   };
+
+  const showList = (_) => {
+    let accHtml = "";
+    read().forEach((acc) => {
+      let temp = html;
+      temp = temp.replaceAll("{{id}}", acc.id);
+      temp = temp.replaceAll("{{AccName}}", acc.AccName);
+      temp = temp.replaceAll("{{Surname}}", acc.AccSurnName);
+      temp = temp.replaceAll("{{Total}}", acc.Sum);
+      accHtml += temp;
+    });
+    ClientList.innerHTML = accHtml;
+  };
+
   //mygtukai
   storeButton.addEventListener("click", (_) => {
     storeLocalHost();
@@ -71,4 +119,6 @@ window.addEventListener("load", (_) => {
     });
   });
   //mygtukai -----
+
+  showList();
 });
