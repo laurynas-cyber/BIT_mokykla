02ba99259b18f6df1ee06dab60a6a5170452c67f
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-05-21 23:04:54 +0300

    homework done

--- homeworkSSRseed/app.js
+++ homeworkSSRseed/app.js
@@ -14,12 +14,30 @@ app.get("/", (req, res) => {
   res.send(html);
 });
 
+// app.get("/seed", (req, res) => {
+//   let html = fs.readFileSync("./data/seed.html", "utf8");
+
+//   res.send(html);
+// });
+
 app.get("/seed", (req, res) => {
   let html = fs.readFileSync("./data/seed.html", "utf8");
-
+  const listItem = fs.readFileSync("./data/listItem.html", "utf8");
+  let data = fs.readFileSync("./data/animals.json", "utf8");
+  data = JSON.parse(data);
+  let listItems = "";
+  data.forEach((li) => {
+    let liHtml = listItem;
+    liHtml = liHtml
+      .replaceAll("{{NAME}}", li.name)
+      .replace("{{AGE}}", li.age)
+      .replace("{{SPECIES}}", li.species);
+    listItems += liHtml;
+  });
+  html = html.replace("{{LI}}", listItems);
   res.send(html);
 });
 
 app.listen(port, (_) => {
-  console.log(`lol app listening on port ${port}`);
+  console.log(`Animals app listening on port ${port}`);
 });
